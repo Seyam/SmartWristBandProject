5f4116f316d7c39edefffb63ae411b9b69ce8fdc
Seyam <seyam.bd.net@gmail.com>
2017-03-05 19:43:12 +0600

    Add files via upload

--- socketio+mqtt_Server(Heroku).js
+++ socketio+mqtt_Server(Heroku).js
@@ -3,29 +3,66 @@
   seyam.bd.net@gmail.com
 ------------------------------ */
 
-// var express = require('express');
-// var app = express();
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 // var fs = require("fs");
-// var bodyParser = require('body-parser');
-// var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 //var sys = require('sys');
 //var net = require('net');
 var sys = require('util');
 var mqtt = require('mqtt'); 
-var io  = require('socket.io')();
+// var io  = require('socket.io')();
 var connections=[];
+
+
+mongoose.connect('mongodb://192.168.10.216/myDB');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log( "we're connected!");
+});
+
+
+
+var mySchema = mongoose.Schema({
+    sdata: String,
+    did  : String,
+    dtime: String
+
+});
+
+var SensorData = mongoose.model('SensorData', mySchema); //use this model to retrieve data from mongoDB
+
+// Create an instance of this above model
+var my_instance = new SensorData({ sdata: 'sonar', did:'11cm', dtime:'9/2/5'}); //Must match the fields or properties of the defined Schema
+
+// Save the new model instance, passing a callback
+// my_instance.save(function (err) {
+//   if (err) return handleError(err);
+//     console.log('Model Saved');// saved!
+// });
+
+app.use(express.static('public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+
+
 
 
 //var broker = new mqtt.MQTTbroker(1883, '127.0.0.1', 'pusher');
 const broker = mqtt.connect('mqtt://iot.eclipse.org')
 broker.subscribe('sonar');
-console.log('subscribed to topic sonar');
+console.log('subscribed to topic \'sonar\'');
 broker.subscribe('feedback');
 console.log('subscribed to topic \'feedback\'');
 
-
-//mongoose.connect('mongodb://192.168.10.216/myDB');
 
 //Multiple sockets requests handler 
 io.sockets.on('connection', function (SocketIOclient) {
@@ -36,34 +73,46 @@ io.sockets.on('connection', function (SocketIOclient) {
   console.log('Connected: '+connections.length+' sockets connected!');
 
 
-  SocketIOclient.on('publish', function (data) {
-      broker.publish(data.topic,data.payload); //NOTICE BOTH TOPIC & PAYLOAD HAVE TO PUBLISH
-      console.log('publishing to '+data.topic+' to turn it '+data.payload);
-  });
-
-
-
-  SocketIOclient.on('subscribe', function (data) {
-        
-        //socket.join(data.topic);
-        broker.subscribe(data.topic);
-        console.log('Subscribing to '+data.topic);
-    });
-
 
   SocketIOclient.on('disconnect',function(){ //No Parameter For Disconnect Event
-    console.log('one socket disconnected!');
+    connections.splice(connections.indexOf(SocketIOclient),1);
+    console.log('Disconnected: %s sockets connected!', connections.length);
   });
+
+
+  // SocketIOclient.on('publish', function (data) {
+  //     broker.publish(data.topic,data.payload); //NOTICE BOTH TOPIC & PAYLOAD HAVE TO PUBLISH
+  //     console.log('publishing to '+data.topic+' to turn it '+data.payload);
+  // });
+
+
+
+  // SocketIOclient.on('subscribe', function (data) {
+        
+  //       //socket.join(data.topic);
+  //       broker.subscribe(data.topic);
+  //       console.log('Subscribing to '+data.topic);
+  // });
 
 });
 
 
 
+
 // // listen to messages coming from the mqtt broker[Not Required Here]
 broker.on('message', function (topic, payload, packet) {
-    console.log(topic+'='+payload);
+    console.log(topic+'='+payload);                                                                                                                            
     io.sockets.emit('mqttData', {'topic':String(topic),
-                            'payload':String(payload)});
+                               'payload':String(payload)});
+
+    var jsonData = JSON.parse(payload);
+
+    db.collection('sensordatas').insert(jsonData, function (err, result) {
+          if (err)
+             console.log('Error');
+          else
+             console.log('Success');
+    });
 });
 
 //For real time state update(addListener way :D :D )
@@ -73,12 +122,26 @@ broker.on('message', function (topic, payload, packet) {
 //                'payload':String(payload)});
 // });
 
-io.listen(5000);
-console.log("SocketIO server is running at port 5000!");
 
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname + '/index.html');
+});
  
-/*broker.addListener('mqttData', function(topic, payload){
-  sys.puts(topic+'='+payload);
-  io.sockets.emit('mqtt',{'topic':String(topic),
-    'payload':String(payload)});
-});*/
+app.get('/data', function (req, res) {
+       //var dataRetrieved = db.getCollection('SensorData').find({});
+       SensorData.find({/*json object key */}, function (err, docs) {
+          //var dt = JSON.parse(docs);
+          console.log(docs);
+          res.json(docs); //perfect!!!
+          //res.end( JSON.stringify(docs));
+          //res.end(docs);
+            // docs.forEach 
+      });
+       
+})
+
+
+
+server.listen(5000);
+console.log("SocketIO server is running at port 5000!");
